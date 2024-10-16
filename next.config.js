eb85db36eaad83552d7b06181ff0394a70e7b041
Kenny Li <limz000609@gmail.com>
2024-10-16 03:54:28 -0500

    deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const { i18n } = require('./next-i18next.config');
 
+const repo = 'my-rank-is-higher-than-yours' // Replace with your repo name
+
 module.exports = {
   reactStrictMode: true,
   i18n: {
     ...i18n,
     defaultLocale: 'zh',
   },
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
 };
